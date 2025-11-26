ae4c9f1e3defa2f26c012f5d7150e48626155fad
David Kalmus <dejvios@seznam.cz>
2025-11-26 14:35:45 +0100

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import {createApp} from 'vue';
+import VueApexCharts from 'vue3-apexcharts';
 
 import App from './App.vue';
 import router from './router';
@@ -6,4 +7,5 @@ import './assets/main.css';
 
 const app = createApp(App);
 app.use(router);
+app.use(VueApexCharts);
 app.mount('#app');
