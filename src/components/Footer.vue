d1ce09ceafd20480d37f9b028b4b555ca7947784
David Kalmus <dejvios@seznam.cz>
2025-11-26 16:30:49 +0100

    Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="border-t border-white/10 text-center text-gray-500 py-3">
+    &copy; {{ currentYear }} Dashboard. All rights reserved.
+  </footer>
+</template>
+
+<script lang="ts" setup>
+  const currentYear = new Date().getFullYear();
+</script>
